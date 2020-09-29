ff06aa222f3b1f2947fe4ad79ad31df0934d5b26
Wyatt Coleman <colemaw3@wwu.edu>
2020-09-29 16:19:59 -0700

    just finished step 3, ball bouncing off of walls

--- js/script.js
+++ js/script.js
@@ -1,20 +1,55 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width/2;
+var y = canvas.height-30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+var ballRadius = 10;
+
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x,y,ballRadius,0,Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw(){
+    //clear previous frame
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    //draw code
+    drawBall();
+    x += dx;
+    y += dy;
+    if(y + dy < ballRadius || y + dy > canvas.height-ballRadius){
+        dy = -dy;
+    }
+    if(x + dx < ballRadius || x + dx > canvas.width-ballRadius){
+        dx = -dx;
+    }
+}
+setInterval(draw,10);
+
+// //Red rectange
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
+
+// //Green Ball
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+// ctx.fillStyle = "green";
+// ctx.fill();
+// ctx.closePath();
+
+// //blue outline rectange
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+// ctx.stroke();
+// ctx.closePath();
